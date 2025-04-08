99e1732c2408ece4724d5ba55ae0b2881a92e9e5
Maicon <maiconluan.lara@yahoo.com.br>
2025-04-07 21:47:27 -0300

    feat: adding assets

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -5,4 +5,6 @@ export { default as oliviaTop } from "./oliviastop.png";
 export { default as oliviaBottom } from "./oliviasbottom.png";   
 export { default as memoriesText } from "./memorias.png";   
 export { default as pretoTop } from "./bannerInvertido.png";   
+export { default as bannerinvertido } from "./bannerInvertido2.png";   
+export { default as gutsCover } from "./gutsCover.jpg";   
 export { default as heroVideo } from "./heroVideo.mp4";   
